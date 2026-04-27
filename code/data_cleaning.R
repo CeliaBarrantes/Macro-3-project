b63a3f6ec5d40d8b85676551b0386e69cb194c64
Celia Barrantes <celia.barrantes.de.san.antonio@gmail.com>
2026-04-27 16:07:44 +0200

    Code actualization

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -1,21 +1,117 @@
 library(dplyr)
-weo <- read.csv("data/raw/WEO_April2026.csv")
-names(weo)
-vars <- c("Gross domestic product (GDP), Current prices, US dollar",
-                            "Population, Persons for countries / Index for country groups",
-                            "Employed persons, Persons for countries / Index for country groups",
-                            "Unemployment rate",
-                            "Change in reserve assets, Net (assets minus liabilities), US dollar",
-                            "Current account balance (credit less debit), US dollar",
-                            "Gross domestic product (GDP), Per capita, purchasing power parity (PPP) international dollar, ICP benchmarks 2017-2021",
-                            "Output gap, Percent of potential GDP",
-                            "Imports of goods and services, US dollar",
-                            "Exports of goods and services, US dollar",
-                            "Gross domestic product (GDP), Current prices, Purchasing power parity (PPP) international dollar, ICP benchmarks 2017-2021",
-                            "WTI crude, Unit prices, US dollars per barrel",
-                            "Gross domestic product (GDP), Current prices, Per capita, US dollar",
-                            "Brent crude, Unit prices, US dollars per barrel",
-                            "Dubai crude, Unit prices, US dollars per barrel")
-weo_small <- weo %>%
-  filter("INDICATOR" %in% vars)
-write.csv(weo_small, "data/processed/weo_clean.csv", row.names = FALSE)
+library(tidyr)
+library(stringr)
+wb <- read.csv("data/raw/WB_extract_2704.csv", header = TRUE, check.names = FALSE)
+View(wb)
+wb_long <- wb %>%
+  rename(
+    country = `Country Name`,
+    country_code = `Country Code`,
+    variable = `Series Name`,
+    variable_code = `Series Code`
+  ) %>%
+  pivot_longer(
+    cols = -c(country, country_code, variable, variable_code),
+    names_to = "year",
+    values_to = "value"
+  ) %>%
+  mutate(
+    year = as.numeric(sub("^(\\d{4}).*", "\\1", year)),
+    value = na_if(value, ".."),
+    value = as.numeric(gsub(",", "", value))
+  )
+  View(wb_long)
+wb_long <- wb_long %>%
+  filter(!is.na(variable), variable != "")
+wb_long <- wb_long %>%
+  mutate(variable = as.character(variable)) %>%
+  mutate(variable = case_when(
+    grepl("GDP \\(current US\\$\\)", variable) ~ "GDP_current",
+    grepl("GDP per capita \\(current US\\$\\)", variable) ~ "GDP_pc_current",
+    grepl("GDP per capita \\(constant 2015 US\\$\\)", variable) ~ "GDP_pc_const",
+    grepl("Imports of goods and services", variable) ~ "Imports",
+    grepl("Exports of goods and services", variable) ~ "Exports",
+    grepl("Current account balance", variable) ~ "CA",
+    grepl("Population, total", variable) ~ "Population",
+    TRUE ~ variable
+  ))
+wb_wide <- wb_long %>%
+  pivot_wider(
+    names_from = variable,
+    values_from = value,
+    values_fn = mean,       # or first()
+    values_fill = NA
+  )
+View(wb_wide)
+
+data_er <- read.csv("data/raw/Exchange_rate_2704.csv", header = TRUE, check.names = FALSE)
+View(data_er)
+data_er_long <- data_er %>%
+  rename(
+    country = `Country Name`,
+    country_code = `Country Code`,
+    variable = `Series Name`,
+    variable_code = `Series Code`
+  ) %>%
+  pivot_longer(
+    cols = -c(country, country_code, variable, variable_code),
+    names_to = "year",
+    values_to = "value"
+  )
+data_er_long <- data_er_long %>%
+mutate(year = as.numeric(str_extract(year, "\\d{4}")))
+View(data_er_long)
+
+data_demo <- read.csv("data/raw/Age_depency_2704.csv", header = TRUE, check.names = FALSE)
+View(data_demo)
+data_demo_long <- data_demo %>%
+  rename(
+    country = `Country Name`,
+    country_code = `Country Code`,
+    variable = `Series Name`,
+    variable_code = `Series Code`
+  ) %>%
+  pivot_longer(
+    cols = -c(country, country_code, variable, variable_code),
+    names_to = "year",
+    values_to = "value"
+  )
+data_demo_long <- data_demo_long %>%
+mutate(year = as.numeric(str_extract(year, "\\d{4}")))
+View(data_demo_long)
+
+data_nfa <- read.csv("data/raw/NFA_2704.csv", header = TRUE, check.names = FALSE)
+View(data_nfa)
+data_nfa_long <- data_nfa %>%
+  rename(
+    country = `Country Name`,
+    country_code = `Country Code`,
+    variable = `Series Name`,
+    variable_code = `Series Code`
+  ) %>%
+  pivot_longer(
+    cols = -c(country, country_code, variable, variable_code),
+    names_to = "year",
+    values_to = "value"
+  )
+data_nfa_long <- data_nfa_long %>%
+mutate(year = as.numeric(str_extract(year, "\\d{4}")))
+View(data_nfa_long)
+
+data_instq <- read.csv("data/raw/Institutional_quality_2704.csv", header = TRUE, check.names = FALSE)
+View(data_instq)
+data_instq_long <- data_instq %>%
+  rename(
+    country = `Country Name`,
+    country_code = `Country Code`,
+    variable = `Series Name`,
+    variable_code = `Series Code`
+  ) %>%
+  pivot_longer(
+    cols = -c(country, country_code, variable, variable_code),
+    names_to = "year",
+    values_to = "value"
+  )
+data_instq_long <- data_instq_long %>%
+mutate(year = as.numeric(str_extract(year, "\\d{4}")))
+View(data_instq_long)
